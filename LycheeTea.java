783cab509c309cf9a0be19a661b5b5dd9355d479
GalihNasasta <141600035+GalihNasasta@users.noreply.github.com>
2024-01-23 11:33:21 +0700

    Add files via upload

---
+++ LycheeTea.java
@@ -0,0 +1,13 @@
+public class LycheeTea extends minuman{
+
+    @Override
+    public void tampilJenis(String jenis){
+        System.out.println("Jenis Minuman: "+jenis);
+    }
+
+    @Override
+    public int jenisMinuman(){
+        return super.jenisMinuman();
+    }
+    
+}
---
+++ tampilMinuman.java
@@ -0,0 +1,8 @@
+public class tampilMinuman {
+
+     public static void main(String[] args) {
+        LycheeTea lt = new LycheeTea();
+        lt.tampilJenis("Tea / Coffee");
+        System.out.println("Harga: "+lt.jenisMinuman());
+     }
+}
